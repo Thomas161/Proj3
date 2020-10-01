a97db9e97b59f38ef65faa7b614568ac5e169c4a
Tom Magin <magintc@mac.com>
2020-10-01 18:48:38 +1000

    #setting up app.js

--- website/app.js
+++ website/app.js
@@ -1,24 +1,32 @@
 /**Global Variables */
-const config = require("./config");
-const fetch = require("cross-fetch");
-const URL = "http://api.openweathermap.org/data/2.5/forecast";
-const API = `APPID${config.API_KEY}`;
-console.log(API); //empty object
-console.log("Fetch URL", URL);
+// const config = require("./config.js");
+// const fetch = require("node-fetch");
+const URL = "http://api.openweathermap.org/data/2.5/weather?";
+const API_KEY = "1a425e11bb04696c4f6f6ab481b9e74d";
+const triggerButton = document.querySelector("#generate");
+// console.log("Fetch API", config); //empty object
+// console.log("Fetch API", API_KEY);
+// console.log("Fetch URL", URL);
 
 /** */
 
 /**Helper functions */
-(async () => {
-  try {
-    const res = await fetch(`${URL}?id=524901&${API}`);
-    console.log(res);
-    if (res.status >= 400) {
-      throw new Error("Bad response");
-    }
-    const retrieved = await res.json();
-    console.log(retrieved);
-  } catch (err) {
-    console.log(err);
-  }
-})();
+
+/**Sample function that tests API key works */
+async function getWeather() {
+  fetch(`${URL}id=524901&appid=${API_KEY}`)
+    .then((res) => res.json())
+    .then((data) => console.log("Name of city =>", data.name))
+    .catch((err) => console.log(err));
+}
+getWeather();
+
+/**Add click event and event listener to button  */
+
+let clickResponse = () => {
+  const zipInput = document.getElementById("zip");
+
+  console.log(zipInput.value);
+};
+clickResponse();
+triggerButton.addEventListener("click", clickResponse);
