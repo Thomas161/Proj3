cf2f799a930eb8925089a1c905e63f8280ed3d26
Tom Magin <magintc@mac.com>
2020-10-01 16:31:49 +1000

    #modified a config file to hold api keys

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-require("dotenv").config();
+const config = require("./website/config");
 const app = express();
-const PORT = process.env.PORT;
+const PORT = 8080;
+let projectData = {};
 
-console.log("PORT in .env =>", process.env.PORT);
-console.log("API Key in .env =>", process.env.API_KEY);
-console.log("ROOT URL in .env =>", process.env.ROOT_URL);
+console.log("API Key in .env =>", config.API_KEY);
+console.log("ROOT URL in .env =>", config.ROOT_URL);
 
 /**Serve up front end */
 app.use(express.static("website"));
@@ -18,9 +18,17 @@ app.use(bodyParser.json());
 app.use(cors);
 /** */
 
-//enable cors for 1 endpoint
-app.get("/", cors(), (req, res) => {
-  res.send("Test Endpoint");
+app.get("/retrieve", (req, res) => {
+  res.send(projectData);
+});
+app.post("/sent", (req, res) => {
+  projectData = {
+    icon: req.body.icon,
+    temp: req.body.temp,
+    date: req.body.date,
+    feelings: req.body.feelings,
+  };
+  res.send("Post sent");
 });
 
 const server = app.listen(PORT, () =>
