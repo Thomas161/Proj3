349d83f0e704b409e7c1f3ef94fe92d49b650838
Tom Magin <magintc@mac.com>
2020-10-02 15:55:59 +1000

    #configuring fecth call to API

--- website/app.js
+++ website/app.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM loaded and parsed", event.timeStamp);
   /**Global Variables */
-  // const config = require("./config.js");
-  // const fetch = require("node-fetch");
+
   const URL = "http://api.openweathermap.org/data/2.5/weather?APPID=";
   const API_KEY = "1a425e11bb04696c4f6f6ab481b9e74d";
   const triggerButton = document.querySelector("#generate");
+  let dateId = document.getElementById("date");
+  let temp = document.getElementById("temp");
+  let content = document.getElementById("content");
+  let entries = document.getElementById("entryHolder");
   // console.log("Fetch API", config); //empty object
   // console.log("Fetch API", API_KEY);
   // console.log("Fetch URL", URL);
@@ -22,19 +25,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
       .then((res) => res.json())
       .then((data) => {
         console.log("Name of city =>", data.name);
-        let con = document.getElementById("content");
-        con.innerHTML = data.name;
+        let date = new Date();
+        dateId.innerHTML = date.getUTCDate();
+        console.log((temp.innerHTML = data.main.temp - 273));
+        content.innerHTML = data.name;
+        entries.style.visibility = "visible";
       })
       .catch((err) => console.log(err));
   }
   getWeather();
   triggerButton.addEventListener("click", getWeather);
+
   /**Add click event and event listener to button  */
+  //   let clickResponse = () => {
+  //     const zipInput = document.getElementById("zip");
 
-  let clickResponse = () => {
-    const zipInput = document.getElementById("zip");
-
-    console.log(zipInput.value);
-  };
-  clickResponse();
+  //     console.log(zipInput.value);
+  //   };
+  //   clickResponse();
 });
