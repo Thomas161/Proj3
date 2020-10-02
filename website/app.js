b566b13404b49618169de44c79452f919f364957
Tom Magin <magintc@mac.com>
2020-10-02 15:33:02 +1000

    #DOMContentLoaded to ensure HTML is loaded and parsed before executing js

--- website/app.js
+++ website/app.js
@@ -1,37 +1,40 @@
-/**Global Variables */
-// const config = require("./config.js");
-// const fetch = require("node-fetch");
-const URL = "http://api.openweathermap.org/data/2.5/weather?APPID=";
-const API_KEY = "1a425e11bb04696c4f6f6ab481b9e74d";
-const triggerButton = document.querySelector("#generate");
-// console.log("Fetch API", config); //empty object
-// console.log("Fetch API", API_KEY);
-// console.log("Fetch URL", URL);
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM loaded and parsed", event.timeStamp);
+  /**Global Variables */
+  // const config = require("./config.js");
+  // const fetch = require("node-fetch");
+  const URL = "http://api.openweathermap.org/data/2.5/weather?APPID=";
+  const API_KEY = "1a425e11bb04696c4f6f6ab481b9e74d";
+  const triggerButton = document.querySelector("#generate");
+  // console.log("Fetch API", config); //empty object
+  // console.log("Fetch API", API_KEY);
+  // console.log("Fetch URL", URL);
 
-/** */
+  /** */
 
-/**Helper functions */
+  /**Helper functions */
 
-/**Sample function that tests API key works */
-async function getWeather() {
-  const zipInput = document.getElementById("zip");
-  const z = zipInput.value;
-  fetch(`${URL}${API_KEY}&q=${z}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("Name of city =>", data.name);
-      let con = document.getElementById("content");
-      con.innerHTML = data.name;
-    })
-    .catch((err) => console.log(err));
-}
-getWeather();
-triggerButton.addEventListener("click", getWeather);
-/**Add click event and event listener to button  */
+  /**Sample function that tests API key works */
+  async function getWeather() {
+    const zipInput = document.getElementById("zip");
+    const z = zipInput.value;
+    fetch(`${URL}${API_KEY}&q=${z}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Name of city =>", data.name);
+        let con = document.getElementById("content");
+        con.innerHTML = data.name;
+      })
+      .catch((err) => console.log(err));
+  }
+  getWeather();
+  triggerButton.addEventListener("click", getWeather);
+  /**Add click event and event listener to button  */
 
-let clickResponse = () => {
-  const zipInput = document.getElementById("zip");
+  let clickResponse = () => {
+    const zipInput = document.getElementById("zip");
 
-  console.log(zipInput.value);
-};
-clickResponse();
+    console.log(zipInput.value);
+  };
+  clickResponse();
+});
