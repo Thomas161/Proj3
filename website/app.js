86d6f4dbc11f733b30b64afd9f765e46bacc98a0
Tom Magin <magintc@mac.com>
2020-10-12 19:09:48 +1100

    #working on fixing the post bug vol15.

--- website/app.js
+++ website/app.js
@@ -38,8 +38,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
           const feelings = feelingsInput;
           const tempMain = parseFloat(convertResponse.main.temp);
           console.log("Temp in Celcius", tempMain);
-          const longitude = `${convertResponse.coord.lon}`;
-          const latitude = `${convertResponse.coord.lat}`;
+          const longitude = convertResponse.coord.lon;
+          const latitude = convertResponse.coord.lat;
           const conditions = convertResponse.weather[0].description;
           console.log("Response json =>", convertResponse);
 
@@ -102,10 +102,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
       console.log("Comes back =>", retrievedData);
       dateEntry.innerHTML = retrievedData.date;
       name.innerHTML = retrievedData.name;
-      temp.innerHTML = Math.round(retrievedData.temp - 273);
+      temp.innerHTML = Math.round(retrievedData.temp - 273) + "&deg;";
       conditions.innerHTML = retrievedData.conditions;
-      lat.innerHTML = retrievedData.latitude;
-      long.innerHTML = retrievedData.longitude;
+      lat.innerHTML = retrievedData.lat;
+      long.innerHTML = retrievedData.long;
       let tempIcon = retrievedData.icon;
       icon.innerHTML = `<img src="http://openweathermap.org/img/w/${tempIcon}.png"; alt="image_weather"/>`;
     } catch (err) {
